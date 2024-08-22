fc7fa5b963622b9c1872ae057fd3610d8de027a8
ThisaraWeerakoon <thisara.weerakoon2001@gmail.com>
2024-08-22 20:59:03 +0530

    uploaded

---
+++ movie_ticket_reservation_system/src/main/java/com/trs/movie_ticket_reservation_system/MovieTicketReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.trs.movie_ticket_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketReservationSystemApplication.class, args);
+	}
+
+}
---
+++ movie_ticket_reservation_system/src/test/java/com/trs/movie_ticket_reservation_system/MovieTicketReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trs.movie_ticket_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
