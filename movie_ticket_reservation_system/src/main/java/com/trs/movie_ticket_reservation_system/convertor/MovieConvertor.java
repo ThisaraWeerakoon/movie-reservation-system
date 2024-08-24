3fc3969946bbce73c43928d23d8bec7f53721737
ThisaraWeerakoon <thisara.weerakoon2001@gmail.com>
2024-08-24 20:22:39 +0530

    movie controller all finished

---
+++ movie_ticket_reservation_system/src/main/java/com/trs/movie_ticket_reservation_system/convertor/MovieConvertor.java
@@ -0,0 +1,19 @@
+package com.trs.movie_ticket_reservation_system.convertor;
+
+import com.trs.movie_ticket_reservation_system.entities.Movie;
+import com.trs.movie_ticket_reservation_system.request.MovieRequest;
+
+public class MovieConvertor {
+    public static Movie movieDtoToMovie(MovieRequest movieRequest) {
+        Movie movie = Movie.builder()
+                .movieName(movieRequest.getMovieName())
+                .duration(movieRequest.getDuration())
+                .genre(movieRequest.getGenre())
+                .language(movieRequest.getLanguage())
+                .releaseDate(movieRequest.getReleaseDate())
+                .rating(movieRequest.getRating())
+                .build();
+
+        return movie;
+    }
+}
--- movie_ticket_reservation_system/src/main/java/com/trs/movie_ticket_reservation_system/services/MovieService.java
+++ movie_ticket_reservation_system/src/main/java/com/trs/movie_ticket_reservation_system/services/MovieService.java
@@ -1,6 +1,8 @@
 package com.trs.movie_ticket_reservation_system.services;
 
+import com.trs.movie_ticket_reservation_system.convertor.MovieConvertor;
 import com.trs.movie_ticket_reservation_system.entities.Movie;
+import com.trs.movie_ticket_reservation_system.exceptions.MovieAlreadyExist;
 import com.trs.movie_ticket_reservation_system.repositories.MovieRepository;
 import com.trs.movie_ticket_reservation_system.request.MovieRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,15 @@ public class MovieService {
         Movie movieByName = movieRepository.findByMovieName(movieRequest.getMovieName());
 
         if(movieByName != null && movieByName.getLanguage().equals(movieRequest.getLanguage())){
+            throw new MovieAlreadyExist();
 
         }
+
+        Movie movie = MovieConvertor.movieDtoToMovie(movieRequest);
+
+        movieRepository.save(movie);
+        return "The movie has been added successfully";
+
 
     }
 }
